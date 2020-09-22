c4dc32d42a9619a73815fb1b05aadc28e18b19f0
Qiao Anran <qiaoanran@gmail.com>
2020-09-22 10:54:58 +0800

    Upgrade Electron to v10.1.2

--- main.js
+++ main.js
@@ -41,7 +41,15 @@ let win
 
 function createWindow () {
   // 创建浏览器窗口。
-  win = new BrowserWindow({width: 1300, height: 800, frame: false })
+  win = new BrowserWindow({
+    width: 1300,
+    height: 800,
+    frame: false,
+    webPreferences: {
+      nodeIntegration: true,
+      enableRemoteModule: true,
+    }
+  })
 
   // 然后加载应用的 index.html。
   win.loadURL(`file://${__dirname}/index.html`);
